3d8d93e09fbc29b245dbeedfd394894ce9273083
Shaurav Zahan Mahmud <shauravzahan91@gmail.com>
2024-04-13 14:36:50 +0600

    Update browseapp.py

--- browseapp.py
+++ browseapp.py
@@ -1,14 +1,6 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 import streamlit.components.v1 as components
 from urllib.parse import urlparse
-
-# Function to fetch and parse HTML content from a URL
-def fetch_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    return soup
 
 # Streamlit app layout
 st.title("Simple Web Browser")
@@ -25,16 +17,10 @@ if st.button("Load"):
             url = "https://" + url
 
         try:
-            # Fetch and parse HTML content from the entered URL
-            soup = fetch_url(url)
-            
-            # Display the parsed HTML content
-            st.write(soup.prettify())
-            
             # Embed the web page using an iframe
             st.write(f"Here's the web page at {url}:")
             st.components.v1.iframe(url, height=600, scrolling=True)
-            
+
         except Exception as e:
             st.error(f"Error loading URL: {e}")
     else:
