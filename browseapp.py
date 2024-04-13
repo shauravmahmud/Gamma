658ba55c6e9b2f070162b420ba67e3c3cdb834dc
Shaurav Zahan Mahmud <shauravzahan91@gmail.com>
2024-04-13 20:21:32 +0600

    Update browseapp.py

--- browseapp.py
+++ browseapp.py
@@ -82,7 +82,7 @@ if st.button("Load"):
             html_content = str(soup)
             st.components.v1.html(
                 html_content,
-                width=1500, height=600, scrolling=True
+                width=1000, height=600, scrolling=True
             )
             
             # Display the clickable links below the HTML content
